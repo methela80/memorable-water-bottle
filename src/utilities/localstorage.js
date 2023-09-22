6b4e90963e172024d42c828fb5e0d9b9009e2c86
methela80 <methela.wd@gmail.com>
2023-09-23 00:24:13 +0600

    add and remove item from cart

---
+++ src/utilities/localstorage.js
@@ -0,0 +1,31 @@
+const getStoredCart = () =>{
+    const storedCartString = localStorage.getItem('cart');
+    if(storedCartString){
+        return JSON.parse(storedCartString)
+    }
+    return [];
+}
+
+const saveCartToLS = cart =>{
+
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified);
+}
+
+
+const addToLS = id =>{
+
+    const cart = getStoredCart();
+    cart.push(id)
+    // saved to local storage  
+    saveCartToLS(cart) 
+}
+
+const removeFromLS = id =>{
+    const cart = getStoredCart();
+    //removing every id
+    const remainingCart = cart.filter(idx => idx !== id);
+    saveCartToLS(remainingCart);
+}
+
+export {addToLS, getStoredCart, removeFromLS}
